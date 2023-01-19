f8f37e4679879151e64a292e141e7a6f77822399
FigureItOut-23 <alwaysdreambig231@gmail.com>
2023-01-18 21:29:17 -0500

    Vishal the goat

--- react-front-end/src/app/App.js
+++ react-front-end/src/app/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const App = () =>{
+const App = () => {
     return(
         <div>
             <h1 className='helloWorld'>HelloWorld!</h1>
+            <p className='helloWorld'>Vishal the goat</p>
         </div>
     );
 };
