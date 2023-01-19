cdbbdcb11ccdfd1f37d3e2ddeda748b861f92cac
VishGit1234 <47926842+VishGit1234@users.noreply.github.com>
2023-01-18 21:13:05 -0500

    Setup react with webpack

---
+++ react-front-end/src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () =>{
+    return(
+        <div>
+            <h1 className='helloWorld'>HelloWorld!</h1>
+        </div>
+    );
+};
+
+export default App;
